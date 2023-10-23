ade0f2284e882b46fda459ea8cd0385fcef40b17
Axel Onofre <96461131+Axel030705@users.noreply.github.com>
2023-10-23 00:11:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("Btn_IRT").addEventListener("click", function () {
+        // Redirigir a otra página
+        window.location.href = "RegistrarTiendas.html";
+    });
+});

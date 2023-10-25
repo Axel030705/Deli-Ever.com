b9950e86440d95c5b554b6d4a52c1503fd270db0
Axel Onofre <96461131+Axel030705@users.noreply.github.com>
2023-10-24 23:45:17 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+/* Boton RegistrarTiendas */
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("Btn_IRT").addEventListener("click", function () {
         // Redirigir a otra página
         window.location.href = "RegistrarTiendas.html";
+    });
+});
+
+/* Boton RegistrarProductos */
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("Btn_IRP").addEventListener("click", function () {
+        // Redirigir a otra página
+        window.location.href = "RegistrarProductos.html";
+    });
+});
+
+/* Boton Editar Tienda y Productos */
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("Btn_ETP").addEventListener("click", function () {
+        // Redirigir a otra página
+        window.location.href = "EditarTiendaProductos.html";
     });
 });

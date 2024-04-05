37d6698bbc6882d30882764bee91f9401528c428
Axel030705 <axel030705@gmail.com>
2024-04-04 23:33:11 -0600

    modified:   indexCliente.css 	modified:   indexCliente.html 	modified:   indexClienteFirebase.js

--- indexClienteFirebase.js
+++ indexClienteFirebase.js
@@ -20,5 +20,49 @@ const analytics = getAnalytics(app);
 // Obtiene una referencia a la base de datos
 const database = getDatabase();
 
-// Obtener una referencia a la lista de usuarios en la base de datos
-const usuariosRef = ref(database, 'Tienda');
+// Obtener una referencia a la lista de tiendas en la base de datos
+const tiendasRef = ref(database, 'Tienda');
+
+// Función para mostrar los datos de los productos en la página
+function mostrarDatosProductos(snapshot) {
+    const contenedorProductos = document.getElementById('containerP');
+    contenedorProductos.innerHTML = ''; // Limpiar el contenedor antes de mostrar nuevos datos
+
+    snapshot.forEach(function (childSnapshot) {
+        const tienda = childSnapshot.val();
+        
+        // Iterar sobre cada producto de la tienda
+        for (const productoId in tienda.productos) {
+            const producto = tienda.productos[productoId];
+            
+            // Crear un contenedor individual para el producto
+            const productoContainer = document.createElement('div');
+            productoContainer.classList.add('producto');
+
+            // Construir el contenido del contenedor
+            const imagen = document.createElement('img');
+            imagen.src = producto.imagenUrl;
+            imagen.classList.add('imagenP')
+            const nombre = document.createElement('h2');
+            nombre.textContent = producto.nombre;
+            const descripcion = document.createElement('p');
+            descripcion.textContent = producto.descripcion;
+            const precio = document.createElement('p');
+            precio.textContent = `Precio: ${producto.precio}`;
+            
+            // Agregar elementos al contenedor del producto
+            productoContainer.appendChild(imagen);
+            productoContainer.appendChild(nombre);
+            productoContainer.appendChild(descripcion);
+            productoContainer.appendChild(precio);
+            
+            // Agregar el contenedor del producto al contenedor principal
+            contenedorProductos.appendChild(productoContainer);
+        }
+    });
+}
+
+// Escuchar cambios en la lista de tiendas y mostrar los datos de los productos en la página
+onValue(tiendasRef, function (snapshot) {
+    mostrarDatosProductos(snapshot);
+});

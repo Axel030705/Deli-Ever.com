b493e932b83c05ebe34033b25faf994aae61a70b
Laptop <michelcrack03@gmail.com>
2024-06-20 08:11:12 -0600

    modified:   Solicitudes.js 	modified:   detalles_producto.js 	modified:   firebase.js 	modified:   indexClienteFirebase.js

--- indexClienteFirebase.js
+++ indexClienteFirebase.js
@@ -3,15 +3,15 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtBtN6py9WVmPz7siJ8jBaAwURLdx_mCg",
-    authDomain: "student-app-fb310.firebaseapp.com",
-    databaseURL: "https://student-app-fb310-default-rtdb.firebaseio.com",
-    projectId: "student-app-fb310",
-    storageBucket: "student-app-fb310.appspot.com",
-    messagingSenderId: "48534996518",
-    appId: "1:48534996518:web:12af5240d14ba2c52888e7",
-    measurementId: "G-C8HV6YSD6X"
-};
+    apiKey: "AIzaSyBPUzD4vFqqtCWQ3Pxf-x9eSWZKT3x2UHY",
+    authDomain: "deli-ever-ee94d.firebaseapp.com",
+    databaseURL: "https://deli-ever-ee94d-default-rtdb.firebaseio.com",
+    projectId: "deli-ever-ee94d",
+    storageBucket: "deli-ever-ee94d.appspot.com",
+    messagingSenderId: "867324946309",
+    appId: "1:867324946309:web:f9c132a70d7f29138a06ec",
+    measurementId: "G-HGHQV4M9KR"
+  };
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
